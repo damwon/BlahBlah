44bd8d729984edc0e09e922bdf54789e12ebe5e6
jodie9596@gmail.com <jodie9596@gmail.com>
2022-04-19 09:36:57 +0900

    chore: react version change

--- FE/blahblah/pages/qna/index.tsx
+++ FE/blahblah/pages/qna/index.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image"
+import { Grid } from "semantic-ui-react"
 export default function QnA() {
   return (
     <>
-      <Image src="/images/meltingPot.PNG" width="1000px" height="200px" className="item" data-aos="fade-up" alt="meltingPot image"></Image>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width = {4}>
+          <Image src="/images/meltingPot.PNG" width="1000px" height="200px" className="item" data-aos="fade-up" alt="meltingPot image"></Image>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      
     </>
   )
 }
