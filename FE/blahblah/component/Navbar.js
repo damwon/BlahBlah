dcacd4e15dfc50092cb6908ce282752f4c258822
kimjiwon <flykimjiwun@naver.com>
2022-04-20 12:06:38 +0900

    [S06P31C203-68] feat:frontNav

--- FE/blahblah/component/Navbar.js
+++ FE/blahblah/component/Navbar.js
@@ -6,27 +6,36 @@ export default function userNav() {
   return (
     <>
     {/* 테스트 */}
-    <Navbar bg="black" variant="dark">
+    <Navbar bg="dark" variant="dark">
     <Container>
     <Link href="/">
  <a className="navmenu">&nbsp;blahblah메인 &nbsp;</a>
   </Link>
     {/* <Navbar.Brand href="/user">user처음페이지</Navbar.Brand> */}
     <Nav className="me-auto">
-    <Link href="/user/friends">
- <a className="navmenu">&nbsp;친구목록 &nbsp;</a>
+    <Link href="/user">
+ <a className="navmenu">&nbsp;User작업 &nbsp;</a>
   </Link>
-  <Link href="/user/login">
- <a className="navmenu">&nbsp;로그인 &nbsp;</a>
+  <Link href="/chat">
+ <a className="navmenu">&nbsp;채팅방 &nbsp;</a>
   </Link>
-  <Link href="/user/mypage">
- <a className="navmenu">&nbsp;마이페이지 &nbsp;</a>
+  <Link href="/wordnote">
+ <a className="navmenu">&nbsp;워드노트 &nbsp;</a>
   </Link>
-  <Link href="/user/signup">
- <a className="navmenu">&nbsp;회원가입 &nbsp;</a>
+  <Link href="/study">
+ <a className="navmenu">&nbsp;스터디 &nbsp;</a>
   </Link>
-  <Link href="/user/updatecheck">
- <a className="navmenu">&nbsp;회원정보수정 &nbsp;</a>
+  <Link href="/qna">
+ <a className="navmenu">&nbsp;QnA &nbsp;</a>
+  </Link>
+  <Link href="/qna/write">
+ <a className="navmenu">&nbsp;Qna작성 &nbsp;</a>
+  </Link>
+  <Link href="/notice">
+ <a className="navmenu">&nbsp;공지 &nbsp;</a>
+  </Link>
+  <Link href="/note">
+ <a className="navmenu">&nbsp;노트 &nbsp;</a>
   </Link>
     </Nav>
     </Container>
