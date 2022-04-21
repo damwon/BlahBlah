429f76e06f13ec939dd8cb47aa34f60fa230e45b
kimjiwon <flykimjiwun@naver.com>
2022-04-21 12:14:03 +0900

    [S06P31C203-70] feat:NavBardesign

--- FE/blahblah/component/Navbar.js
+++ FE/blahblah/component/Navbar.js
@@ -5,15 +5,17 @@ export default function userNav() {
   return (
     <>
       {/* 테스트 */}
-      <div>
+      <div className="nav">
       <Link href="/">
-            <a className="navmenu">&nbsp;메인 &nbsp;</a>
+            <a className="main">
+              <img src="logo/blbl.png" height={30}></img>
+              </a>
           </Link>
             <Link href="/chat">
               <a className="navmenu">&nbsp;채팅 &nbsp;</a>
             </Link>
             <Link href="/wordnote">
-              <a className="navmenu">&nbsp;워드노트 &nbsp;</a>
+              <a className="navmenu">&nbsp;워드 &nbsp;</a>
             </Link>
             <Link href="/study">
               <a className="navmenu">&nbsp;스터디 &nbsp;</a>
@@ -31,13 +33,13 @@ export default function userNav() {
               <a className="navmenu">&nbsp;노트 &nbsp;</a>
             </Link>
             <Link href="/user/friends">
- <a className="navmenu">&nbsp;친구목록 &nbsp;</a>
+ <a className="navmenu">&nbsp;친구 &nbsp;</a>
   </Link>
   <Link href="/user/login">
- <a className="navmenu">&nbsp;로그인 &nbsp;</a>
+ <a className="navmenu">&nbsp;로긴 &nbsp;</a>
   </Link>
   <Link href="/user/mypage">
- <a className="navmenu">&nbsp;마이페이지 &nbsp;</a>
+ <a className="navmenu">&nbsp;mypage &nbsp;</a>
   </Link>
   <Link href="/user/signup">
  <a className="navmenu">&nbsp;가입 &nbsp;</a>
@@ -47,12 +49,25 @@ export default function userNav() {
   </Link>
       </div>
       <style jsx>{`
+      .nav{
+        margin-top:15px;
+      }
+        .main{
+          color:black;
+          font-size;24px;
+          text-decoration-line: none;
+          display:inline-block;
+        }
         .navmenu {
           color: black;
           text-decoration-line: none;
           display: inline-block;
           margin: 0px;
-          font-size: 12px;
+          font-size: 16px;
+          font-weight:bold;
+        }
+        .navmenu:hover{
+          color:#00ccb1;
         }
       `}</style>
     </>
