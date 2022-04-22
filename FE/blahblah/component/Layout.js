d4f81fdc2f74d9ec4b964992091cd85ebf1bd3b7
kimjiwon <flykimjiwun@naver.com>
2022-04-22 10:25:37 +0900

    feat:좌우여백

--- FE/blahblah/component/Layout.js
+++ FE/blahblah/component/Layout.js
@@ -5,14 +5,14 @@ export default function Layout( {children}){
   return <>
   <Container>
   <Row>
-    <Col sm={1} xs={1}></Col>
+    {/* <Col sm={1} xs={1}></Col> */}
     <Col><NavBar></NavBar></Col>
-    <Col sm={1} xs={1}></Col>
+    {/* <Col sm={1} xs={1}></Col> */}
   </Row>
   <Row>
-    <Col sm={1} xs={1}></Col>
+    {/* <Col sm={1} xs={1}></Col> */}
     <Col><div>{children}</div></Col>
-    <Col sm={1} xs={1}></Col>
+    {/* <Col sm={1} xs={1}></Col> */}
   </Row>
 </Container>
   
