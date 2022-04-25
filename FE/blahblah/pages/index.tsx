eb22aac8a19879bb03592a1d985f7a35809d3e71
kimjiwon <flykimjiwun@naver.com>
2022-04-25 18:17:06 +0900

    fe/feat/mainIndex

--- FE/blahblah/pages/index.tsx
+++ FE/blahblah/pages/index.tsx
@@ -1,33 +1,11 @@
 import { useEffect,useState } from "react";
 export default function Home() {
 
-  const [test,setTest] = useState(false)
-  useEffect(() => {
-    const token = localStorage.getItem("jwt");
-    console.log('로그인후 메인페이지에서 jwt읽기')
-    console.log(localStorage.getItem("jwt"))
-    if (token) {
-      setTest(true)
-    }
-  }, []);
-
-  const onLogout = (event:any) => {
-    event.preventDefault();
-    setTest(false);
-    localStorage.removeItem("jwt");
-    console.log(localStorage.getItem("key"))
-    // history.push("/login");
-  };
   
 
   return <>
   <h1>블라블라 메인페이지</h1>
-  {
-    test===true
-    ?<>로그인상태</>
-    :<>비로그인상태</>
-  }
-  <button onClick={onLogout}>로그아웃</button>
+
   
 
   </>
