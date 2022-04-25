9045cc731d21ff4fcc5e05e4aff68b9773ec247e
kimjiwon <flykimjiwun@naver.com>
2022-04-25 15:19:18 +0900

    fe/feat/mypage

--- FE/blahblah/pages/user/mypage.tsx
+++ FE/blahblah/pages/user/mypage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container,Row,Col,ListGroup,Figure } from 'react-bootstrap';
 
 export default function Mypage() {
   return (
@@ -7,12 +7,45 @@ export default function Mypage() {
       
       <Container>
         <Row>
+        <Col>
+      
+      </Col>
       <Col>
-      <h1>마이페이지</h1>
+      <div className="mypage">
+      <Figure>
+  <Figure.Image
+    width={171}
+    height={180}
+    src="/user/young-man.png"
+  />
+</Figure>
+      </div>
+      <ListGroup variant="flush">
+  <ListGroup.Item>이름:</ListGroup.Item>
+  <ListGroup.Item>이메일주소</ListGroup.Item>
+  <ListGroup.Item>모국어:</ListGroup.Item>
+  <ListGroup.Item>구사언어:</ListGroup.Item>
+  <ListGroup.Item>학습언어:</ListGroup.Item>
+  <ListGroup.Item>성별:</ListGroup.Item>
+  <ListGroup.Item>나이:</ListGroup.Item>
+  <ListGroup.Item>자기소개:</ListGroup.Item>
+</ListGroup>
+      </Col>
+      <Col>
+      
       </Col>
         </Row>
   
     </Container>
+    <style jsx>{`
+        
+        .mypage {
+          display: flex;
+  align-items: center;
+  justify-content: center;
+        }
+
+      `}</style>
     </>
   )
 }
