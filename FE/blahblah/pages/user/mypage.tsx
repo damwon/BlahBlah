540e7a34263c65c31bd049d4b9c7e50cc5afc74f
김지원 <flykimjiwun@naver.com>
2022-04-28 11:22:09 +0900

    [S06P31C203-204]fe/feat/MypageLoginHandle

--- FE/blahblah/pages/user/mypage.tsx
+++ FE/blahblah/pages/user/mypage.tsx
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import { Container,Row,Col,ListGroup,Figure ,Card,Button} from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Figure, Card, Button } from 'react-bootstrap';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Token } from '@mui/icons-material';
 
 export default function Mypage() {
+
+  const [islogin, setIslogin] = useState(false)
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      setIslogin(true)
+    } else {
+      alert('로그인상태가아냐!')
+      router.push('/')
+    }
+  }, []);
   const router = useRouter()
   const [profile, setProfile] = useState<any>([]);
   // 타입스크립트 설정.. ㅠㅡㅠ
@@ -28,100 +40,102 @@ export default function Mypage() {
     return config;
   };
   useEffect(() => {
-    getProfile();
-  }, []);
+    if (islogin) {
+      getProfile();
+    }
+  }, [islogin]);
   return (
     <>
-      
+
       <Container>
         <Row>
-        <Col sm = {3} sx = {3}>
-        <Card style={{ width: '16rem',margin:'20px'  }}>
-  <Card.Body>
-    <Card.Title>친구목록</Card.Title>
-    <ListGroup variant="flush">
-    <ListGroup.Item>최근친구1</ListGroup.Item>
-    <ListGroup.Item>최근친구2</ListGroup.Item>
-    <ListGroup.Item>최근친구3</ListGroup.Item>
-  </ListGroup>
-  <a className="linkmenu" onClick={()=>{
-    router.push('/user/friends')
-  }}>전체친구 보러가기</a>
-  </Card.Body>
-</Card>
+          <Col sm={3} sx={3}>
+            <Card style={{ width: '16rem', margin: '20px' }}>
+              <Card.Body>
+                <Card.Title>친구목록</Card.Title>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>최근친구1</ListGroup.Item>
+                  <ListGroup.Item>최근친구2</ListGroup.Item>
+                  <ListGroup.Item>최근친구3</ListGroup.Item>
+                </ListGroup>
+                <a className="linkmenu" onClick={() => {
+                  router.push('/user/friends')
+                }}>전체친구 보러가기</a>
+              </Card.Body>
+            </Card>
 
-<Card style={{ width: '16rem',margin:'20px' }}>
-  <Card.Body>
-    <Card.Title>내가 작성한 피드</Card.Title>
-    <ListGroup variant="flush">
-    <ListGroup.Item>최근작성글1</ListGroup.Item>
-    <ListGroup.Item>최근작성글</ListGroup.Item>
-    <ListGroup.Item>최근작성글</ListGroup.Item>
-  </ListGroup>
-  <a className="linkmenu">전체목록 보러가기</a>
-  </Card.Body>
-</Card>
-      </Col>
-      <Col sm = {6} sx = {6}>
-      <div className="mypage">
-      <Figure>
-  <Figure.Image
-    width={171}
-    height={180}
-    src="/user/young-man.png"
-  />
-</Figure>
-      </div>
-      <ListGroup variant="flush">
-  <ListGroup.Item>이름:{profile.name}</ListGroup.Item>
-  <ListGroup.Item>이메일주소:{profile.email}</ListGroup.Item>
-  <ListGroup.Item>모국어:</ListGroup.Item>
-  <ListGroup.Item>구사언어:</ListGroup.Item>
-  <ListGroup.Item>학습언어:</ListGroup.Item>
-  <ListGroup.Item>성별:{profile.gender===0
-  ?<>여자</>
-  :<>남자</>
-  }</ListGroup.Item>
-  <ListGroup.Item>나이:{profile.age}</ListGroup.Item>
-  <ListGroup.Item>자기소개:{profile.description}</ListGroup.Item>
-  <Button onClick={()=>{
-    router.push('/user/profileupdate')
-  }}
-  style={{ marginBottom:'20px' }} variant="outline-dark">프로필수정</Button>
-  <Button onClick={()=>{
-    router.push('/user/passupdate')
-  }}
-  style={{ marginBottom:'20px' }} variant="outline-dark">비밀번호수정</Button>
-</ListGroup>
-      </Col>
-      <Col sm = {3} sx = {3}>
-      <Card style={{ width: '16rem',margin:'20px' }}>
-  <Card.Body>
-    <Card.Title>단어장</Card.Title>
-    <ListGroup variant="flush">
-    <ListGroup.Item>최근추가한 단어1</ListGroup.Item>
-    <ListGroup.Item>최근추가한 단어2</ListGroup.Item>
-    <ListGroup.Item>최근추가한 단어3</ListGroup.Item>
-  </ListGroup>
-  <a className="linkmenu">전체단어 보러가기</a>
-  </Card.Body>
-</Card>
-<Card style={{ width: '16rem',margin:'20px' }}>
-  <Card.Body>
-    <Card.Title>문의내역</Card.Title>
-    <ListGroup variant="flush">
-    <ListGroup.Item>최근 문의 내역1</ListGroup.Item>
-    <ListGroup.Item>최근 문의 내역2</ListGroup.Item>
-    <ListGroup.Item>최근 문의 내역3</ListGroup.Item>
-  </ListGroup>
-  <a className="linkmenu">전체문의 보러가기</a>
-  </Card.Body>
-</Card>
-      </Col>
+            <Card style={{ width: '16rem', margin: '20px' }}>
+              <Card.Body>
+                <Card.Title>내가 작성한 피드</Card.Title>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>최근작성글1</ListGroup.Item>
+                  <ListGroup.Item>최근작성글</ListGroup.Item>
+                  <ListGroup.Item>최근작성글</ListGroup.Item>
+                </ListGroup>
+                <a className="linkmenu">전체목록 보러가기</a>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={6} sx={6}>
+            <div className="mypage">
+              <Figure>
+                <Figure.Image
+                  width={171}
+                  height={180}
+                  src="/user/young-man.png"
+                />
+              </Figure>
+            </div>
+            <ListGroup variant="flush">
+              <ListGroup.Item>이름:{profile.name}</ListGroup.Item>
+              <ListGroup.Item>이메일주소:{profile.email}</ListGroup.Item>
+              <ListGroup.Item>모국어:</ListGroup.Item>
+              <ListGroup.Item>구사언어:</ListGroup.Item>
+              <ListGroup.Item>학습언어:</ListGroup.Item>
+              <ListGroup.Item>성별:{profile.gender === 0
+                ? <>여자</>
+                : <>남자</>
+              }</ListGroup.Item>
+              <ListGroup.Item>나이:{profile.age}</ListGroup.Item>
+              <ListGroup.Item>자기소개:{profile.description}</ListGroup.Item>
+              <Button onClick={() => {
+                router.push('/user/profileupdate')
+              }}
+                style={{ marginBottom: '20px' }} variant="outline-dark">프로필수정</Button>
+              <Button onClick={() => {
+                router.push('/user/passupdate')
+              }}
+                style={{ marginBottom: '20px' }} variant="outline-dark">비밀번호수정</Button>
+            </ListGroup>
+          </Col>
+          <Col sm={3} sx={3}>
+            <Card style={{ width: '16rem', margin: '20px' }}>
+              <Card.Body>
+                <Card.Title>단어장</Card.Title>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>최근추가한 단어1</ListGroup.Item>
+                  <ListGroup.Item>최근추가한 단어2</ListGroup.Item>
+                  <ListGroup.Item>최근추가한 단어3</ListGroup.Item>
+                </ListGroup>
+                <a className="linkmenu">전체단어 보러가기</a>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '16rem', margin: '20px' }}>
+              <Card.Body>
+                <Card.Title>문의내역</Card.Title>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>최근 문의 내역1</ListGroup.Item>
+                  <ListGroup.Item>최근 문의 내역2</ListGroup.Item>
+                  <ListGroup.Item>최근 문의 내역3</ListGroup.Item>
+                </ListGroup>
+                <a className="linkmenu">전체문의 보러가기</a>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
-  
-    </Container>
-    <style jsx>{`
+
+      </Container>
+      <style jsx>{`
         
         .mypage {
           display: flex;
