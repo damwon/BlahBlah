d6bc41b2db15978b82a9a7c1a1276a9c3182ea3c
김지원 <flykimjiwun@naver.com>
2022-04-28 13:08:57 +0900

    [feat]fe/feat/signupDesign

--- FE/blahblah/pages/user/findpass.tsx
+++ FE/blahblah/pages/user/findpass.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable */
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 export default function Findpass() {
+  const router = useRouter()
 
   const [email, setEmail] = useState("");
-  const handleEmail = (event:any) => {
+  const handleEmail = (event: any) => {
     event.preventDefault();
     setEmail(event.target.value);
   };
-  const onCheckEmail = (event:any) => {
+  const onCheckEmail = (event: any) => {
     event.preventDefault();
     axios({
       url: `https://blahblah.community:8443/api/user/find-password`,
@@ -29,19 +31,27 @@ export default function Findpass() {
   };
   return (
     <>
-       <Container>
+      <Container>
         <Row>
-      <Col>
-      <h1>비밀번호찾기</h1>
-      이메일입력해
-      {email}
-      <input onChange={handleEmail}></input>
-      <button onClick={onCheckEmail}>눌러버튼</button>
-      </Col>
+          <Col>
+            <h1>비밀번호찾기</h1>
+            이메일입력해
+            {email}
+            <div>
+              <input onChange={handleEmail}></input>
+            </div>
+            <Button onClick={onCheckEmail}
+              style={{ marginBottom: '20px' }} variant="outline-dark">인증메일보내기</Button>
+            <Button onClick={() => {
+              router.push('/user/login')
+            }}
+              style={{ marginBottom: '20px' }} variant="outline-dark">로그인하러가기</Button>
+            {/* <button onClick={onCheckEmail}>눌러버튼</button> */}
+          </Col>
         </Row>
-  
-    </Container>
-      
+
+      </Container>
+
     </>
   )
 }
