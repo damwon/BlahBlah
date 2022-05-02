70ab0af6f0d6d3cb866d04a468cdde7a8e090867
dsw00513 <dsw00513@naver.com>
2022-05-02 10:09:39 +0900

    [S06P31C203-228]Feat: language&user 조회 API 및 언어정보 API

--- BE/src/main/java/com/ssafy/blahblah/api/service/language/LanguageService.java
+++ BE/src/main/java/com/ssafy/blahblah/api/service/language/LanguageService.java
@@ -4,6 +4,8 @@ package com.ssafy.blahblah.api.service.language;
 import com.ssafy.blahblah.db.entity.Language;
 import com.ssafy.blahblah.db.entity.User;
 
+import java.util.List;
+
 /**
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
@@ -11,4 +13,5 @@ public interface LanguageService {
 	Language createLanguage(String code, String Langimg, String engName);
 	Language getLanguageByCode(String code);
 	Language getLanguageByEngName(String engName);
+	List<Language> getLanguageTable();
 }
