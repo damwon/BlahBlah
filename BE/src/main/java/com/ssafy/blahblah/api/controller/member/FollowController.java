61bf1e5f979e69d122df1445efd3ba4765eaef3a
고재현 <joali9807@naver.com>
2022-05-12 14:06:48 +0900

    [S06P31C203-297]Fix: 이미지 업로드 수정

--- BE/src/main/java/com/ssafy/blahblah/api/controller/member/FollowController.java
+++ BE/src/main/java/com/ssafy/blahblah/api/controller/member/FollowController.java
@@ -2,13 +2,11 @@ package com.ssafy.blahblah.api.controller.member;
 
 import com.ssafy.blahblah.api.response.member.FollowerListRes;
 import com.ssafy.blahblah.api.response.member.FollowingListRes;
-import com.ssafy.blahblah.api.response.member.ReportListRes;
 import com.ssafy.blahblah.api.service.member.FollowService;
 import com.ssafy.blahblah.api.service.member.UserService;
 import com.ssafy.blahblah.common.auth.SsafyUserDetails;
 import com.ssafy.blahblah.db.entity.Follow;
 import com.ssafy.blahblah.db.entity.User;
-import com.ssafy.blahblah.db.repository.FollowRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
