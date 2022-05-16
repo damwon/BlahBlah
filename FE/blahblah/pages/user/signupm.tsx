2b17ce212b01070a9b1f60a5ba0906ef3ba144cb
kimjiwon <flykimjiwun@naver.com>
2022-05-16 12:21:55 +0900

    fe/feat/SignUp

--- FE/blahblah/pages/user/signupm.tsx
+++ FE/blahblah/pages/user/signupm.tsx
@@ -231,8 +231,8 @@ export default function Signup() {
     ]
     // let test = [{ "code": "kor", "level": 3 }, { "code": "eng", "level": 4 }, { "code": "chi", "level": 5 }]
     const info: any = {
-      "email":'23test2233@test.com',
-      // "email": query.email,
+      // "email":'23test2233@test.com',
+      "email": query.email,
       "name": name,
       "gender": gen,
       "age": age,
