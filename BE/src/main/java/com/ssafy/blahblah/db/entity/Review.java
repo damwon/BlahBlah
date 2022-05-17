0c7320d0a1549135ed23b4181ad68c5510f7d7d4
dsw00513 <dsw00513@naver.com>
2022-05-17 12:07:34 +0900

    [S06P31C203-330]Feat: reviewAPI

--- BE/src/main/java/com/ssafy/blahblah/db/entity/Review.java
+++ BE/src/main/java/com/ssafy/blahblah/db/entity/Review.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -25,10 +26,14 @@ public class Review extends BaseEntity{
     @Column(nullable = false)
     private String reviewTxt;
 
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
     @Builder
-    public Review(Long reviewUserId, Long userId, String reviewTxt) {
+    public Review(Long reviewUserId, Long userId, String reviewTxt, LocalDateTime createdAt) {
         this.reviewUserId = reviewUserId;
         this.userId = userId;
         this.reviewTxt = reviewTxt;
+        this.createdAt = createdAt;
     }
 }
